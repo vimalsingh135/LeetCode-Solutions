c215a6926b486cc0d79287767b9b0b207925036f
Vimal Singh <157205996+vimalsingh135@users.noreply.github.com>
2026-08-14 20:26:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (52.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        prev2, prev1 = 1, 2
+        for _ in range(3, n + 1):
+            prev2, prev1 = prev1, prev2 + prev1
+        return prev1
